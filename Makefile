b42d540727a14d382fb8f3773932ba5834ee9758
Rogier Velting <rogiervelting@gmail.com>
2016-10-28 14:35:58 +0200

    threading added

--- Makefile
+++ Makefile
@@ -1,5 +1,12 @@
 HEADERS=src/light.hpp src/node.hpp
 SOURCES=src/main.cpp src/light.cpp src/node.cpp
+CXXFLAGS+=-std=c++11 -pthread -Isrc
 
 all: $(HEADERS) $(SOURCES)
-	g++ $(SOURCES) -Isrc -o bin/controller
+	g++ $(SOURCES) $(CXXFLAGS) -o bin/controller
+
+run: all
+	bin/controller
+
+clean: 
+	rm bin/*
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
 #include "node.hpp"
 #include "light.hpp"
 
 #define br if(false) break;
 
-int main(int argc, char const *argv[])
-{
-
-	light l = light(Node(2,3));
-	while(true) {
+void main_loop(bool* running) {
+	while(*running) {
+		// std::cout << "loop" <<std::endl;
 		br;
 		// check for websocket writes
 
@@ -15,5 +16,16 @@ int main(int argc, char const *argv[])
 
 		// send update signals
 	}
+}
+
+int main(int argc, char const *argv[])
+{
+	light l = light(Node(2,3));
+	bool main_run = true;
+	std::thread main_thread(main_loop, &main_run);
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	main_run = false;
+	main_thread.join();
+	
 	return 0;
 }
