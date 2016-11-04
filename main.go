d535267ffa12a3e79e7484c0e005236edc2293be
Rogier Velting <rogiervelting@gmail.com>
2016-11-04 18:28:04 +0100

    JSON

--- main.go
+++ main.go
@@ -6,12 +6,37 @@ import (
 	"github.com/gorilla/websocket"
 	"fmt"
 	"log"
+	"encoding/json"
+	"bytes"
 )
 
+type ControllerState struct {
+	State []ControllerStateSub `json=state`
+}
+
+type ControllerStateSub struct {
+	TrafficLight int `json=trafficLight`
+	Status string `json=status`
+}
+
+type SimulatorState struct {
+	State []SimulatorStateSub `json=state`
+} 
+
+type SimulatorStateSub struct {
+	TrafficLight int `json=trafficLight`
+	Count int `json=count`
+}
+
 var addr = flag.String("addr", "0.0.0.0:3000", "http service address")
-var upgrader = websocket.Upgrader{} // use default options
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true},
+}
 
 func echo(w http.ResponseWriter, r *http.Request) {
+	var simState SimulatorState
+	var contrState ControllerState
+
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Print("upgrade:", err)
@@ -27,7 +52,16 @@ func echo(w http.ResponseWriter, r *http.Request) {
 		log.Printf("recv: %s", message)
 
 		// LOGIC
-		
+		inDec := json.NewDecoder(bytes.NewReader(message))
+		err = inDec.Decode(&simState)
+		if err != nil {
+			log.Fatal(err)
+		}
+		contrState = ControllerState{ State: []ControllerStateSub{ ControllerStateSub{TrafficLight: simState.State[0].TrafficLight, Status:"green"} }  }
+		message, err = json.Marshall(&contrState)
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		err = c.WriteMessage(mt, message)
 		if err != nil {
