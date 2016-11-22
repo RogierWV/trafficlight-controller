1bfe1f481d4fb3328a292c4140536951877bb9ae
Rogier Velting <rogiervelting@gmail.com>
2016-11-22 18:27:03 +0100

    Reintroduced timer

--- timer.go
+++ timer.go
@@ -4,33 +4,6 @@ import (
 	"time"
 )
 
-// func time_to_yellow(id <-chan int, out chan<- bool, state chan<- StateModCommand, red chan<- int) {
-// 	for {
-// 		tlid := <- id
-// 		time.Sleep(3*time.Second)
-// 		red<-tlid
-// 		state <- StateModCommand {
-// 			false,
-// 			func(contrState *ControllerState, ret chan<- ControllerState){(*contrState).State[tlid].Status = "yellow"},
-// 			nil,
-// 		}
-// 		out <- true
-// 	}
-// }
-
-// func time_to_red(id <-chan int, out chan<- bool, state chan<- StateModCommand) {
-// 	for {
-// 		tlid := <- id
-// 		time.Sleep(3*time.Second)
-// 		state <- StateModCommand {
-// 			false,
-// 			func(contrState *ControllerState, ret chan<- ControllerState){(*contrState).State[tlid].Status = "red"},
-// 			nil,
-// 		}
-// 		out <- true
-// 	}
-// }
-
 func _timer(grID int, out chan<- bool, state chan<- StateModCommand, colour string) {
 	time.Sleep(3*time.Second)
 	state <- StateModCommand {
