3d3304cb868e293fdad087b733bd6ad023b2eb91
Rogier Velting <rogiervelting@gmail.com>
2016-11-22 18:07:04 +0100

    Reworked selection algorithm

--- lights.go
+++ lights.go
@@ -1,6 +1,6 @@
 package main
 
-var light_simultanous = [][]int {
+var lightGroups = [][]int {
 	{4,10,23,24,33,34},
 	{2,6,42},
 }
--- process.go
+++ process.go
@@ -6,43 +6,78 @@ import (
 )
 
 func process_simstate(msg <-chan []byte, out chan<- bool, state chan<- StateModCommand, timer chan<- int) {
-	var simState SimulatorState
+	simState := SimulatorState{make([]SimulatorStateSub, 50)}
+	for i := 0; i < len(simState.State); i++ {
+		simState.State[i] = SimulatorStateSub{i,0}
+	}
 
-	retChan := make (chan ControllerState)
+	// retChan := make (chan ControllerState)
 
 	for {
 		message := <-msg
 
-		err := json.Unmarshal(message, &simState)
+		var tmpSimState SimulatorState
+
+		err := json.Unmarshal(message, &tmpSimState)
 		if err != nil {
 			log.Println(err)
 		}
 
-		green := SimulatorStateSub{-1,0}
+		for _,e := range tmpSimState.State {
+			simState.State[e.TrafficLight] = e
+		}
 
-		//set a green light
-		for _,e := range simState.State {
-			if e.Count > green.Count {
-				green = e
+		// green := SimulatorStateSub{-1,0}
+
+		// //set a green light
+		// for _,e := range simState.State {
+		// 	if e.Count > green.Count {
+		// 		green = e
+		// 	}
+		// }
+
+		// if green.TrafficLight != -1 {
+		// 	state <- StateModCommand{
+		// 		false,
+		// 		func(contrState *ControllerState, ret chan<- ControllerState){
+		// 			// set_all_red(contrState)
+		// 				(*contrState).State[green.TrafficLight] = ControllerStateSub{green.TrafficLight,"green"}
+		// 			ret <- (*contrState)
+		// 		},
+		// 		retChan,
+		// 	}
+
+		// 	timer <- green.TrafficLight
+		// }
+
+		// state <- StateModCommand{true,nil,retChan}
+		highestTotal := 0
+		groupId := -1
+		// currState := <-retChan
+
+		for i := 0; i < len(lightGroups); i++ {
+			total := 0
+			for j := 0; j < len(lightGroups[i]); j++ {
+				total += simState.State[lightGroups[i][j]].Count 
+			}
+			if total > highestTotal {
+				highestTotal = total
+				groupId = i
 			}
 		}
 
-		if green.TrafficLight != -1 {
-			state <- StateModCommand{
+		if groupId != -1 {
+			state <- StateModCommand {
 				false,
 				func(contrState *ControllerState, ret chan<- ControllerState){
-					// set_all_red(contrState)
-						(*contrState).State[green.TrafficLight] = ControllerStateSub{green.TrafficLight,"green"}
-					ret <- (*contrState)
+					for _,e := range lightGroups[groupId] {
+						(*contrState).State[e] = ControllerStateSub{e,"green"}
+					}
 				},
-				retChan,
+				nil,
 			}
 
-			timer <- green.TrafficLight
 		}
-
-		// contrState := <-retChan
-
 		
 		out <- true
 	}
